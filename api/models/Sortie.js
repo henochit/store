6214aae461ef2a7e340bde971434577fbdccc8b7
henock <htshibanda@cmd.cd>
2019-09-15 12:49:10 +0100

    motif

--- api/models/Sortie.js
+++ api/models/Sortie.js
@@ -10,6 +10,7 @@ module.exports = {
   attributes: {
      date: { type: 'ref', columnType: 'datetime'},
      quantite: 'number',
+     motif: 'string',
     createdAt: { type: 'ref', columnType: 'datetime', autoCreatedAt: true, },
     updatedAt: { type: 'ref', columnType: 'datetime', autoUpdatedAt: true, },
 
